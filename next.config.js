e8f595d02914c5ce4d710275bdcaa2331761ce1e
afalito <andres.felipe.arguello21@gmail.com>
2024-11-12 21:06:23 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   async headers() {
     return [
       {
@@ -8,9 +10,26 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src https://www.youtube.com"
           },
-          // Otras cabeceras de seguridad...
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
         ],
       },
     ]
   },
 }
+
+module.exports = nextConfig
