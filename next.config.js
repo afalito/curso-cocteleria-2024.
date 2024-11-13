60ddabf7e1166bc5b87258759451dd9e0e8c6e20
afalito <andres.felipe.arguello21@gmail.com>
2024-11-12 21:04:49 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,35 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   async headers() {
     return [
       {
         source: '/:path*',
         headers: [
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; frame-src https://www.youtube.com"
-          }
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src https://www.youtube.com"
+          },
+          // Otras cabeceras de seguridad...
         ],
       },
     ]
   },
 }
-
-module.exports = nextConfig
